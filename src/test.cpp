0439da6d6cf1242c9e7955bff24aeca56ea1cd3b
wilkie <wilkie05@gmail.com>
2012-02-24 16:14:56 -0500

    Test application opens png tileset.

--- src/test.cpp
+++ src/test.cpp
@@ -9,6 +9,7 @@ int main(int argc, char** argv) {
 	settings.resolutionX = WIDTH;
 	settings.resolutionY = HEIGHT;
 	Badger::Engine* engine = new Badger::Engine(&settings);
+	Badger::SpriteSheet* sheet = new Badger::SpriteSheet("assets/floortiles.png");
 	engine->run();
 
 	return 0;
