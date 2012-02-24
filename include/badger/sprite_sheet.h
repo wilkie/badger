b5b633aa59dba49bae7512f93663c50da4cfba5e
wilkie <wilkie05@gmail.com>
2012-02-24 16:04:37 -0500

    Opens PNG file in SpriteSheet.

--- include/badger/sprite_sheet.h
+++ include/badger/sprite_sheet.h
@@ -1,11 +1,15 @@
 #ifndef BADGER_SPRITE_SHEET_H
 #define BADGER_SPRITE_SHEET_H
 
+#include <SDL_image.h>
+
 namespace Badger {
 	class SpriteSheet {
 		private:
+			SDL_Surface* _image;
+
 		public:
-			SpriteSheet();
+			SpriteSheet(const char* filename);
 	};
 }
 
--- src/sprite_sheet.cpp
+++ src/sprite_sheet.cpp
@@ -1,4 +1,11 @@
 #include "badger/sprite_sheet.h"
 
-Badger::SpriteSheet::SpriteSheet() {
+Badger::SpriteSheet::SpriteSheet(const char* filename) {
+	SDL_RWops   *rwop;
+	rwop   = SDL_RWFromFile(filename, "rb");
+	_image = IMG_LoadPNG_RW(rwop);
+	if (_image == NULL) {
+		char* error = IMG_GetError();
+		fprintf(stderr, "IMG_LoadPNG_RW: %s\n", error);
+	}
 }
