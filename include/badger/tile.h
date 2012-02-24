90c1c4043364fef5f4b7ed300144c8c63b10e80b
wilkie <wilkie05@gmail.com>
2012-02-24 15:42:17 -0500

    Adds Tile struct.

---
+++ include/badger/tile.h
@@ -0,0 +1,12 @@
+#ifndef BADGER_TILE_H
+#define BADGER_TILE_H
+
+namespace Badger {
+	struct Tile {
+		bool         passable;
+		bool         destructable;
+		unsigned int spriteIndex;
+	};
+}
+
+#endif
