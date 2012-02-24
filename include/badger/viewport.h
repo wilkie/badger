988c3ab8c207da3f3714d58a16daf2d29d4fe989
wilkie <wilkie05@gmail.com>
2012-02-24 16:28:16 -0500

    Viewport holds a Map reference.

--- include/badger/viewport.h
+++ include/badger/viewport.h
@@ -1,10 +1,18 @@
 #ifndef BADGER_VIEWPORT_H
 #define BADGER_VIEWPORT_H
 
+#include "badger/map.h"
+#include "badger/renderer.h"
+
 namespace Badger {
 	class Viewport {
+		private:
+			Map* _map;
 		public:
 			Viewport();
+			void map(Badger::Map* value);
+			Badger::Map* map();
+			void draw(Renderer* renderer);
 	};
 }
 
--- src/viewport.cpp
+++ src/viewport.cpp
@@ -2,3 +2,14 @@
 
 Badger::Viewport::Viewport() {
 }
+
+void Badger::Viewport::draw(Renderer* renderer) {
+}
+
+Badger::Map* Badger::Viewport::map() {
+	return _map;
+}
+
+void Badger::Viewport::map(Badger::Map* value) {
+	_map = value;
+}
