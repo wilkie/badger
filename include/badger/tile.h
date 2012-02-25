c23d060b5304db998dafb230bb9fe9bc3de84b70
wilkie <wilkie05@gmail.com>
2012-02-25 13:43:18 -0500

    Adds documentation to Tile.

--- include/badger/tile.h
+++ include/badger/tile.h
@@ -2,9 +2,23 @@
 #define BADGER_TILE_H
 
 namespace Badger {
+  /*
+   *  Defines a tile on the map.
+   */
   struct Tile {
+    /*
+     *  Whether or not the tile can be walked through.
+     */
     bool         passable;
+
+    /*
+     *  Whether or not the tile can be destroyed.
+     */
     bool         destructable;
+
+    /*
+     *  The sprite index in the Badger::SpriteSheet to draw for this tile.
+     */
     unsigned int spriteIndex;
   };
 }
