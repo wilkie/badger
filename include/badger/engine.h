d6de43821295adf9386b2de1b73c07da2c1df9b6
wilkie <wilkie05@gmail.com>
2012-02-25 13:41:02 -0500

    Adds documentation to Engine.

--- include/badger/engine.h
+++ include/badger/engine.h
@@ -12,30 +12,61 @@
 #endif
 
 namespace Badger {
+  /*
+   *  Describes the display configuration the Engine should attempt to use.
+   */
   struct VideoSettings {
+    /*
+     *  The horizontal resolution.
+     */
     unsigned int resolutionX;
+
+    /*
+     *  The vertical resolution.
+     */
     unsigned int resolutionY;
   };
 
+  /*
+   *  Handles rendering and most major logic.
+   */
   class Engine {
     public:
+      /*
+       *  Construct an Engine initialized with the given display configuration
+       */
       Engine(VideoSettings* video);
+
+      /*
+       *  Attach the given Badger::Map.
+       */
       void map(Map* value);
 
+      /*
+       *  Execute the game loop. There is no escape from this function until the game ends.
+       */
       void run();
 
     private:
+
+      // Internal storage
       VideoSettings _video;
       Renderer*     _renderer;
       Viewport*     _mainViewport;
       Map*          _map;
 
+      // Initialization methods
       bool _startSDL();
       bool _initialize();
       void _initViewport();
+
+      // Input event handler
       void _fireEvent(void* data);
 
+      // Called to draw a frame
       void _draw();
+
+      // Called to update all game objects
       void _update();
   };
 }
