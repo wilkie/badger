690487ee48f147462161d9e0cd3d4a688c39abc0
wilkie <wilkie05@gmail.com>
2012-02-25 14:32:12 -0500

    Truncates long lines in Renderer.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -101,7 +101,8 @@ void Badger::Renderer::drawArrays(const float vertices[],
   glDisableClientState(GL_NORMAL_ARRAY);
 }
 
-void Badger::Renderer::drawSquare(float x, float y, float width, float height, double tu, double tv, double tw, double th) {
+void Badger::Renderer::drawSquare(float x, float y, float width, float height,
+                                  double tu, double tv, double tw, double th) {
   // square ////////////////////////////////////////////////////////////////////
   //  v1------v0
   //  |       |
@@ -110,15 +111,19 @@ void Badger::Renderer::drawSquare(float x, float y, float width, float height, d
   //  v2------v3
 
   // vertex coords array
-  static const float vertices[] = {1,1,0,  -1,1,0,  -1,-1,0,  1,-1,0};       // v0-v1-v2-v3
+  static const float vertices[] = {
+    1, 1, 0, -1, 1, 0, -1, -1, 0, 1, -1, 0}; // v0-v1-v2-v3
 
   // normal array
-  static const float normals[] = {0,0,1,  0,0,1,  0,0,1,  0,0,1};            // v0-v1-v2-v3
+  static const float normals[] = {
+    0, 0, 1,  0, 0, 1,  0,  0, 1, 0,  0, 1}; // v0-v1-v2-v3
 
   // color array
-  static const float colors[] = {0.72f,0.54f,0,  0.72f,0.54f,0,  0.52f,0.388f,0,  0.72f,0.388f,0}; // v0-v1-v2-v3
+  static const float colors[] = {
+    0.7f, 0.5f, 0, 0.7f, 0.5f, 0, 0.5f, 0.4f, 0, 0.7f, 0.4f, 0}; // v0-v1-v2-v3
 
-  double texture_coords[] = {tu+tw, tv, tu, tv, tu, tv+th, tu+tw, tv+th}; // v0-v1-v2-v3
+  double texture_coords[] = {
+    tu+tw, tv, tu, tv, tu, tv+th, tu+tw, tv+th}; // v0-v1-v2-v3
 
   static const unsigned char indices[] = {0, 1, 2, 3};
 
@@ -136,8 +141,9 @@ void Badger::Renderer::drawSquare(float x, float y, float width, float height, d
   glPopMatrix();
 }
 
-void Badger::Renderer::drawCube(float x, float y, float z, float width, float height, float depth) {
-  // cube ///////////////////////////////////////////////////////////////////////
+void Badger::Renderer::drawCube(float x, float y, float z,
+                                float width, float height, float depth) {
+  // cube //////////////////////////////////////////////////////////////////////
   //    v6----- v5
   //   /|      /|
   //  v1------v0|
@@ -147,35 +153,39 @@ void Badger::Renderer::drawCube(float x, float y, float z, float width, float he
   //  v2------v3
 
   // vertex coords array
-  static const float vertices[] = {1,1,1,  -1,1,1,  -1,-1,1,  1,-1,1,        // v0-v1-v2-v3
-              1,1,1,  1,-1,1,  1,-1,-1,  1,1,-1,        // v0-v3-v4-v5
-              1,1,1,  1,1,-1,  -1,1,-1,  -1,1,1,        // v0-v5-v6-v1
-              -1,1,1,  -1,1,-1,  -1,-1,-1,  -1,-1,1,    // v1-v6-v7-v2
-              -1,-1,-1,  1,-1,-1,  1,-1,1,  -1,-1,1,    // v7-v4-v3-v2
-              1,-1,-1,  -1,-1,-1,  -1,1,-1,  1,1,-1};   // v4-v7-v6-v5
+  static const float vertices[] = {
+     1,  1,  1, -1,  1,  1, -1, -1,  1,  1, -1,  1,   // v0-v1-v2-v3
+     1,  1,  1,  1, -1,  1,  1, -1, -1,  1,  1, -1,   // v0-v3-v4-v5
+     1,  1,  1,  1,  1, -1, -1,  1, -1, -1,  1,  1,   // v0-v5-v6-v1
+    -1,  1,  1, -1,  1, -1, -1, -1, -1, -1, -1,  1,   // v1-v6-v7-v2
+    -1, -1, -1,  1, -1, -1,  1, -1,  1, -1, -1,  1,   // v7-v4-v3-v2
+     1, -1, -1, -1, -1, -1, -1,  1, -1,  1,  1, -1};  // v4-v7-v6-v5
 
   // normal array
-  static const float normals[] = {0,0,1,  0,0,1,  0,0,1,  0,0,1,             // v0-v1-v2-v3
-             1,0,0,  1,0,0,  1,0,0, 1,0,0,              // v0-v3-v4-v5
-             0,1,0,  0,1,0,  0,1,0, 0,1,0,              // v0-v5-v6-v1
-             -1,0,0,  -1,0,0, -1,0,0,  -1,0,0,          // v1-v6-v7-v2
-             0,-1,0,  0,-1,0,  0,-1,0,  0,-1,0,         // v7-v4-v3-v2
-             0,0,-1,  0,0,-1,  0,0,-1,  0,0,-1};        // v4-v7-v6-v5
+  static const float normals[] = {
+     0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,   // v0-v1-v2-v3
+     1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,  0,   // v0-v3-v4-v5
+     0,  1,  0,  0,  1,  0,  0,  1,  0,  0,  1,  0,   // v0-v5-v6-v1
+    -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0,   // v1-v6-v7-v2
+     0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1,  0,   // v7-v4-v3-v2
+     0,  0, -1,  0,  0, -1,  0,  0, -1,  0,  0, -1};  // v4-v7-v6-v5
 
   // color array
-  static const float colors[] = {0.72f,0.54f,0,  0.72f,0.54f,0,  0.52f,0.388f,0,  0.72f,0.388f,0, // v0-v1-v2-v3
-            0.72f,0.54f,0,  0.72f,0.54f,0,  0.52f,0.388f,0,  0.72f,0.388f,0, // v0-v3-v4-v5
-            1,1,1,  0,1,1,  0,1,0,  1,1,0,                                   // v0-v5-v6-v1
-            0.72f,0.54f,0,  0.72f,0.54f,0,  0.52f,0.388f,0,  0.72f,0.388f,0, // v1-v6-v7-v2
-            0,0,0,  0,0,1,  1,0,1,  1,0,0,                                   // v7-v4-v3-v2
-            0.72f,0.54f,0,  0.72f,0.54f,0,  0.52f,0.388f,0,  0.72f,0.388f,0};// v4-v7-v6-v5
+  static const float colors[] = {
+    0.7f, 0.5f, 0, 0.7f, 0.5f, 0, 0.52f, 0.4f, 0, 0.7f, 0.4f, 0, // v0-v1-v2-v3
+    0.7f, 0.5f, 0, 0.7f, 0.5f, 0, 0.52f, 0.4f, 0, 0.7f, 0.4f, 0, // v0-v3-v4-v5
+       1,    1, 1,    0,    1, 1,     0,    1, 0,    1,    1, 0, // v0-v5-v6-v1
+    0.7f, 0.5f, 0, 0.7f, 0.5f, 0, 0.52f, 0.4f, 0, 0.7f, 0.4f, 0, // v1-v6-v7-v2
+       0,    0, 0,    0,    0, 1,     1,    0, 1,    1,    0, 0, // v7-v4-v3-v2
+    0.7f, 0.5f, 0, 0.7f, 0.5f, 0, 0.52f, 0.4f, 0, 0.7f, 0.4f, 0};// v4-v7-v6-v5
 
-  static const unsigned char indices[] = {0,1,2,3,
-             4,5,6,7,
-             8,9,10,11,
-             12,13,14,15,
-             16,17,18,19,
-             20,21,22,23};
+  static const unsigned char indices[] = {
+     0,  1,  2,  3,
+     4,  5,  6,  7,
+     8,  9, 10, 11,
+    12, 13, 14, 15,
+    16, 17, 18, 19,
+    20, 21, 22, 23};
 
   glCullFace(GL_BACK);
 
