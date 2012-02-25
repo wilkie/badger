568195e94815fd8ef7e9a1476e421c404dad42b0
wilkie <wilkie05@gmail.com>
2012-02-25 13:48:14 -0500

    Adds documentation to Map.

--- include/badger/map.h
+++ include/badger/map.h
@@ -5,23 +5,48 @@
 #include "badger/sprite_sheet.h"
 
 namespace Badger {
+  /*
+   *  Describes a world made out of tiles drawn from a single SpriteSheet.
+   */
   class Map {
-    private:
-      unsigned int _width;
-      unsigned int _height;
-      Tile*        _tiles;
-
-      SpriteSheet* _spriteSheet;
-
     public:
+      /*
+       *  Constructs a Badger::Map of the given width and height and drawn
+       *    using the given Badger::SpriteSheet
+       */
       Map(unsigned int width,
           unsigned int height,
           SpriteSheet* spriteSheet);
 
+      /*
+       *  Queries the tile at the world coordinates (x,y)
+       */
       Tile*        tile(unsigned int x, unsigned int y);
+
+      /*
+       *  Returns the width of the map.
+       */
       unsigned int width();
+
+      /*
+       *  Returns the height of the map
+       */
       unsigned int height();
+
+      /*
+       *  Returns the Badger::SpriteSheet being used to draw the map.
+       */
       SpriteSheet* spriteSheet();
+
+    private:
+
+      // Internal storage
+      unsigned int _width;
+      unsigned int _height;
+      SpriteSheet* _spriteSheet;
+
+      // The tiles that compose the map
+      Tile*        _tiles;
   };
 }
 
