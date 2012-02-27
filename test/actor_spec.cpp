91aa9986163bcd5577dcbd16875dec65d84d1070
wilkie <wilkie05@gmail.com>
2012-02-26 21:34:07 -0500

    A working Actor test specification.

--- test/actor_spec.cpp
+++ test/actor_spec.cpp
@@ -1,17 +1,27 @@
 #include "harness.h"
 
-#include "../include/badger/actor.h"
+#include "badger/actor.h"
 
 describe (Actor) {
   describe (position) {
-    it ("should yield the x coordinate described in the given actor file") {
-      Actor* a = new Actor("files/blob.actor", NULL, 0, 0);
-      should(a->position.x == 16);
+    it ("should yield the x coordinate specified when it was constructed") {
+      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      should(a->position().x == 42);
     }
 
-    it ("should yield the y coordinate described in the given actor file") {
-      Actor* a = new Actor("files/blob.actor", NULL, 0, 0);
-      should(a->position.y == 11);
+    it ("should yield the y coordinate specified when it was constructed") {
+      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      should(a->position().y == 24);
+    }
+
+    it ("should yield the width described in the given actor file") {
+      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      should(a->position().width == 16);
+    }
+
+    it ("should yield the height described in the given actor file") {
+      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      should(a->position().height == 11);
     }
   }
 }
