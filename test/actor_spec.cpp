a1eb1c569028a223c020fc47643c7e273f0d1905
wilkie <wilkie05@gmail.com>
2012-02-26 21:55:51 -0500

    Actor spec uses project root as working directory.

--- test/actor_spec.cpp
+++ test/actor_spec.cpp
@@ -6,22 +6,22 @@
 describe (Actor) {
   describe (position) {
     it ("should yield the x coordinate specified when it was constructed") {
-      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
       should(a->position().x == 42);
     }
 
     it ("should yield the y coordinate specified when it was constructed") {
-      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
       should(a->position().y == 24);
     }
 
     it ("should yield the width described in the given actor file") {
-      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
       should(a->position().width == 16);
     }
 
     it ("should yield the height described in the given actor file") {
-      Badger::Actor* a = new Badger::Actor("files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
       should(a->position().height == 11);
     }
   }
