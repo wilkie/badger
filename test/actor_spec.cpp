3a64842a06d41d5e3f462789798387f42e4743b8
jackie kircher <kircher.jackie@gmail.com>
2012-03-03 18:43:38 -0500

    match actor_spec to new actor constructor

--- test/actor_spec.cpp
+++ test/actor_spec.cpp
@@ -13,22 +13,22 @@ void Badger::SpriteSheet::textureCoordinates(unsigned int index, double coords[4
 describe (Actor) {
   describe (position) {
     it ("should yield the x coordinate specified when it was constructed") {
-      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", 42, 24);
       should(a->position().x == 42);
     }
 
     it ("should yield the y coordinate specified when it was constructed") {
-      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", 42, 24);
       should(a->position().y == 24);
     }
 
     it ("should yield the width described in the given actor file") {
-      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", 42, 24);
       should(a->position().width == 16);
     }
 
     it ("should yield the height described in the given actor file") {
-      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", NULL, 42, 24);
+      Badger::Actor* a = new Badger::Actor("test/files/blob.actor", 42, 24);
       should(a->position().height == 11);
     }
   }
